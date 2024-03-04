e32e4d5b24e51e5d25e5a3961d991f863eeac859
Parth Sharma <parthsharma@csu.fullerton.edu>
2024-03-04 07:28:17 -0800

    March 4 Morning Update

--- PythonTest.py
+++ PythonTest.py
@@ -1,4 +1,40 @@
 #new python file 
 
+"""
+#Date: 03/03/2024
 # First
 print("Hello, World!")
+
+#indentation
+if 5>2:
+    print("You are right, five is greater than two.")
+     
+# four is common, && they have to be in the same line
+
+
+#variable 
+#they are created when you assign a value
+x=5
+y="Hello, hello beautiful people!!"
+
+print(x)
+print(y)
+#variable type can be changed easily by just chaging the value
+#so 
+x=y
+print(x)
+
+
+#comments
+#it doesn't have a multi line comments but instead useing triple unassigned string like three quotes 
+#and that block gets ignored.
+
+"""
+
+#Date: 03/04/2024
+
+#casting variables assigns type to the data
+
+x = str("Hello")
+print(x)
+
