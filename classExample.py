40633190929023929d9e5d4f052505d351b0098f
Parth Sharma <parthsharma@csu.fullerton.edu>
2024-08-21 09:53:17 -0700

    making class variable

--- classExample.py
+++ classExample.py
@@ -1,20 +1,31 @@
 class Employee:
 
-    # constructor
+    raise_amount = 1.05
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@gmail.com"
 
-# creating method to print full name
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    def pay_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
 
 # creating instance
 emp_1 = Employee("Parth", "Sharma", 60000)
+emp_2 = Employee("raj", "malhotra", 60000)
 
 # calling instance values
 print(emp_1.email)
 print(emp_1.fullname())
+
+emp_1.raise_amount = 1.06
+emp_1.pay_raise()
+print(emp_1.pay)
+
+emp_2.pay_raise()
+print(emp_2.pay)
