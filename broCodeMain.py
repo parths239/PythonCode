0711bdb0ea933b4e8e2671500e6b4211028bb83d
Parth Sharma <parthsharma@csu.fullerton.edu>
2026-07-24 09:53:07 -0700

    finished the brocode crash course

---
+++ broCodeMain.py
@@ -0,0 +1,16 @@
+# this is a comment 
+print("\n I like Pizza \n")
+
+
+full_name = "Parth Sanjaykumar Sharma"
+age = 28
+gpa = 3.2
+is_student = True
+
+print(f"Hello, {full_name}. You are {age} years old and your gpa is {gpa}\n")
+print(f"are you a student?: {is_student}\n")
+
+if is_student:
+  print("He is student")
+else:
+  print("He is not a student")
--- classExample.py
+++ classExample.py
@@ -1,7 +1,7 @@
 class Employee:
 
     raise_amount = 1.05
-
+    
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
